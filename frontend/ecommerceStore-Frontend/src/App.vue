8bc2021029c94633eaea894811fdf75642413a4f
casoriano03 <casoriano03@gmail.com>
2025-03-06 22:06:47 +0100

    initial commit - frontend

---
+++ frontend/ecommerceStore-Frontend/src/App.vue
@@ -0,0 +1,12 @@
+<script setup>
+import Navbar from './components/Navbar.vue';
+import Hero from './components/Hero.vue';
+import Footer from './components/Footer.vue';
+</script>
+
+<template>
+ <Navbar/>
+ <Hero/>
+ <Footer/>
+ 
+</template>
